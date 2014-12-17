404776713d71c1afd505e10e4a77ae4d7d63b58e
Florian Lopes <florian.lopes@outlook.com>
2014-12-17 13:23:30 +0100

    Updated config to match Heroku env

--- src/main/java/fr/florianlopes/cocktail/api/Application.java
+++ src/main/java/fr/florianlopes/cocktail/api/Application.java
@@ -4,10 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
+@EnableSpringDataWebSupport
+@EnableJpaRepositories(basePackages = "fr.florianlopes.cocktail.api.repository")
 public class Application {
 
     public static void main(String[] args) {
--- src/main/java/fr/florianlopes/cocktail/api/model/Cocktail.java
+++ src/main/java/fr/florianlopes/cocktail/api/model/Cocktail.java
@@ -1,7 +1,69 @@
 package fr.florianlopes.cocktail.api.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.UUID;
+
 /**
  * Created by flopes on 16/12/2014.
  */
+@Entity
+@Table(name = "cocktail")
 public class Cocktail {
+
+	@Id
+	private UUID id;
+
+	private String cocktailId;
+
+	private String name;
+
+	private String description;
+
+	private String price;
+
+	public Cocktail() {
+		this.id = UUID.randomUUID();
+	}
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public String getCocktailId() {
+		return cocktailId;
+	}
+
+	public void setCocktailId(String cocktailId) {
+		this.cocktailId = cocktailId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getPrice() {
+		return price;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
 }
